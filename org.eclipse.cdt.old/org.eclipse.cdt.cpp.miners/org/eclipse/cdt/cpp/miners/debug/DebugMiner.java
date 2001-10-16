d40cbaaeecc308f414a2d26559a0d6653fc27e05
David McKnight <dmcknigh@ca.ibm.com>
2001-10-16 21:37:02 +0000

    cont

--- org.eclipse.cdt.old/org.eclipse.cdt.cpp.miners/org/eclipse/cdt/cpp/miners/debug/DebugMiner.java
+++ org.eclipse.cdt.old/org.eclipse.cdt.cpp.miners/org/eclipse/cdt/cpp/miners/debug/DebugMiner.java
@@ -13,7 +13,7 @@ public class DebugMiner extends Miner
     
     public void load()
     {  
-	_debugJarPath = _dataStore.getAttribute(DataStoreAttributes.A_PLUGIN_PATH) + "com.ibm.debug.gdb.Gdb/derdebug.jar";
+	_debugJarPath = _dataStore.getAttribute(DataStoreAttributes.A_PLUGIN_PATH) + "com.ibm.debug.temp/derdebug.jar";
 	_debugInvocation = "java -cp " + _debugJarPath + " com.ibm.debug.gdb.Gdb -qhost=localhost ";
     }
         
@@ -43,13 +43,13 @@ public class DebugMiner extends Miner
 	String invocationStr = _debugInvocation + "-quiport=" + port.getName() + " -startupKey=" + key.getName(); 
 	DataElement invocation = _dataStore.createObject(null, "invocation", invocationStr);
 
+	System.out.println("doing " + invocation);
 	DataElement cmdDescriptor = _dataStore.localDescriptorQuery(directory, "C_COMMAND");
 	if (cmdDescriptor != null)
 	    {
 		ArrayList args = new ArrayList();
 		args.add(invocation);
 
-		System.out.println("doing " + invocation);
 		_dataStore.command(cmdDescriptor, invocation, directory);
 	    }
     }
