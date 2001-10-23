75ce2f1198d44463801d83e116ae7674dec4cd2f
David McKnight <dmcknigh@ca.ibm.com>
2001-10-23 14:01:36 +0000

    first open source contribution from as400

--- org.eclipse.cdt.old/org.eclipse.cdt.dstore.core/org/eclipse/cdt/dstore/core/server/Server.java
+++ org.eclipse.cdt.old/org.eclipse.cdt.dstore.core/org/eclipse/cdt/dstore/core/server/Server.java
@@ -9,40 +9,47 @@ import com.ibm.dstore.core.server.*;
 public class Server
 {
   private ConnectionEstablisher  _establisher;
-
-  public static void main(String[] args)
-      {
-	Server theServer = null;
-	
-        if (args.length > 0)
-	  {
-	    if (args.length > 1)
-	      {
-		theServer = new Server(args[0], args[1]);	      
-	      }
-	    else
-	      {
-		theServer = new Server(args[0]);	      	      
-	      }
-	  }	
-        else
-	  {
-	    theServer = new Server();
-	  }
-	
-	theServer.run();
-      }
-
-  public Server() 
-      {
+    
+    public static void main(String[] args)
+    {
+	try 
+	    {
+		Server theServer = null;
+		
+		if (args.length > 0)
+		    {
+			if (args.length > 1)
+			{
+			    theServer = new Server(args[0], args[1]);	      
+			}
+			else
+			    {
+				theServer = new Server(args[0]);	      	      			    }
+		    }	
+		else
+		    {
+			theServer = new Server();
+		    }
+		
+		theServer.run();
+	    }
+	catch (SecurityException e) 
+	    {
+		System.err.println(ServerReturnCodes.RC_SECURITY_ERROR);
+		throw e;   // Optional
+	    }
+    }
+    
+    public Server() 
+    {
         _establisher = new ConnectionEstablisher();
-      }
-
-  public Server(String port)
-      {
+    }
+    
+    public Server(String port)
+    {
         _establisher = new ConnectionEstablisher(port);
-      } 
-
+    } 
+    
     public Server(String port, String ticket)
     {
 	_establisher = new ConnectionEstablisher(port, ticket);
