8f4a919e6a8ff1827737ed8738aba6db1f943aef
David McKnight <dmcknigh@ca.ibm.com>
2001-10-29 20:51:53 +0000

    no more core dependency

--- org.eclipse.cdt.old/org.eclipse.cdt.dstore.extra/org/eclipse/cdt/dstore/extra/setup.bat
+++ org.eclipse.cdt.old/org.eclipse.cdt.dstore.extra/org/eclipse/cdt/dstore/extra/setup.bat
@@ -1,10 +1,7 @@
 set ECLIPSEDIR=e:\eclipse\eclipse
-set CORE=%ECLIPSEDIR%\plugins\com.ibm.dstore.core
 set EXTRA=%ECLIPSEDIR%\plugins\com.ibm.dstore.extra
-set A_PLUGIN_PATH=%CORE%
-set MINERS=%ECLIPSEDIR%\plugins\com.ibm.dstore.miners
 
-set CLASSPATH=%CLASSPATH%;%ECLIPSEDIR%\plugins\org.eclipse.core.runtime\runtime.jar;%ECLIPSEDIR%\plugins\org.eclipse.core.boot\boot.jar;%ECLIPSEDIR%\plugins\org.eclipse.core.resources\resources.jar;%ECLIPSEDIR%\plugins\org.eclipse.ui\workbench.jar;%ECLIPSEDIR%\plugins\org.eclipse.swt\swt.jar;%ECLIPSEDIR%\startup.jar;%ECLIPSEDIR%\plugins\com.ibm.lpex\eclpiselpex.jar;%CORE%;%EXTRA%;.;%MINERS%
+set CLASSPATH=%CLASSPATH%;%ECLIPSEDIR%\plugins\org.eclipse.core.runtime\runtime.jar;%ECLIPSEDIR%\plugins\org.eclipse.core.boot\boot.jar;%ECLIPSEDIR%\plugins\org.eclipse.core.resources\resources.jar;%ECLIPSEDIR%\plugins\org.eclipse.ui\workbench.jar;%ECLIPSEDIR%\plugins\org.eclipse.swt\swt.jar;%ECLIPSEDIR%\startup.jar;%ECLIPSEDIR%\plugins\com.ibm.lpex\eclpiselpex.jar;%EXTRA%;.;%MINERS%
 
 set PATH=%PATH%;%ECLIPSEDIR%\..\jre\bin;%ECLIPSEDIR%;	
 
