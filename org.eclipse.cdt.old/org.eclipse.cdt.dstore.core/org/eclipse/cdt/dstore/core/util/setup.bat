fe5d400ae4a9707ac17226253693f04cbcef99c4
Jeff Turnham <turnham@ca.ibm.com>
2001-11-07 16:28:37 +0000

    *** empty log message ***

--- org.eclipse.cdt.old/org.eclipse.cdt.dstore.core/org/eclipse/cdt/dstore/core/util/setup.bat
+++
@@ -1,7 +0,0 @@
-set ECLIPSEDIR=d:\eclipse\itp
-set CPP=%ECLIPSEDIR%\plugins\com.ibm.cpp.core
-set A_PLUGIN_PATH=%CPP%
-
-set CLASSPATH=%CLASSPATH%;%ECLIPSEDIR%\plugins\org.eclipse.core.runtime\runtime.jar;%ECLIPSEDIR%\plugins\org.eclipse.core.boot\boot.jar;%ECLIPSEDIR%\plugins\org.eclipse.core.resources\resources.jar;%ECLIPSEDIR%\plugins\org.eclipse.ui\workbench.jar;%ECLIPSEDIR%\startup.jar;%ECLIPSEDIR%\plugins\com.ibm.lpex\vabaselpex.jar;%CPP%;.;%ECLIPSEDIR%\plugins\com.ibm.etools.C;
-set PATH=%PATH%;%ECLIPSEDIR%\..\jre\bin;%ECLIPSEDIR%;	
-
--- org.eclipse.cdt.old/org.eclipse.cdt.dstore.extra/org/eclipse/cdt/dstore/extra/setup.bat
+++
@@ -1,7 +0,0 @@
-set ECLIPSEDIR=e:\eclipse\eclipse
-set EXTRA=%ECLIPSEDIR%\plugins\com.ibm.dstore.extra
-
-set CLASSPATH=%CLASSPATH%;%ECLIPSEDIR%\plugins\org.eclipse.core.runtime\runtime.jar;%ECLIPSEDIR%\plugins\org.eclipse.core.boot\boot.jar;%ECLIPSEDIR%\plugins\org.eclipse.core.resources\resources.jar;%ECLIPSEDIR%\plugins\org.eclipse.ui\workbench.jar;%ECLIPSEDIR%\plugins\org.eclipse.swt\swt.jar;%ECLIPSEDIR%\startup.jar;%ECLIPSEDIR%\plugins\com.ibm.lpex\eclpiselpex.jar;%EXTRA%;.;%MINERS%
-
-set PATH=%PATH%;%ECLIPSEDIR%\..\jre\bin;%ECLIPSEDIR%;	
-
