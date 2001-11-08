b9fc4af2a2b8a6f898e367173f6ce803fcd16e2d
David McKnight <dmcknigh@ca.ibm.com>
2001-11-08 17:23:47 +0000

    server classpath update

--- org.eclipse.cdt.old/org.eclipse.cdt.dstore.core/win.env.bat
+++ org.eclipse.cdt.old/org.eclipse.cdt.dstore.core/win.env.bat
@@ -3,6 +3,7 @@ set CORE=%ECLIPSEDIR%\plugins\com.ibm.dstore.core
 set MINERS=%ECLIPSEDIR%\plugins\com.ibm.dstore.miners
 set EXTRA=%ECLIPSEDIR%\plugins\com.ibm.dstore.extra\dstore_extra_server.jar
 set CPP_MINERS=%ECLIPSEDIR%\plugins\com.ibm.cpp.miners
+set PARSER_MINER=%ECLIPSEDIR%\plugins\com.ibm.cpp.miners.parser
 
 set A_PLUGIN_PATH=%ECLIPSEDIR%\plugins\
-set CLASSPATH=%CORE%;%MINERS%;%CPP_MINERS%;%EXTRA%;%CLASSPATH%
+set CLASSPATH=%CORE%;%MINERS%;%CPP_MINERS%;%PARSER_MINER%;%EXTRA%;%CLASSPATH%
