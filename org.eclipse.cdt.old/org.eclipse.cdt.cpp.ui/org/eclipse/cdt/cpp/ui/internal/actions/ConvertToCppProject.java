1b714004b9f7790dff785739f157e9de512e2c0f
David McKnight <dmcknigh@ca.ibm.com>
2001-11-16 17:40:47 +0000

    conver to cpp

---
+++ org.eclipse.cdt.old/org.eclipse.cdt.cpp.ui/org/eclipse/cdt/cpp/ui/internal/actions/ConvertToCppProject.java
@@ -0,0 +1,120 @@
+package com.ibm.cpp.ui.internal.actions;
+
+/*
+ * Copyright (c) 2001 International Business Machines Corporation. All rights reserved.
+ * This program and the accompanying materials are made available under the terms of
+ * the Common Public License which accompanies this distribution.
+ */
+
+import com.ibm.cpp.ui.internal.*;
+import com.ibm.cpp.ui.internal.dialogs.*;
+import com.ibm.cpp.ui.internal.api.*;
+
+import org.eclipse.ui.*;
+
+import org.eclipse.jface.viewers.*;
+import org.eclipse.jface.action.*;
+
+import org.eclipse.swt.*;
+import org.eclipse.swt.widgets.*;
+
+import org.eclipse.core.runtime.*;
+import org.eclipse.core.resources.*;
+
+
+import java.io.*;
+import java.util.*;
+
+public class ConvertToCppProject implements IActionDelegate, ISelectionChangedListener 
+{
+    private IProject      _project= null;
+    private CppPlugin     _plugin;
+
+    public ConvertToCppProject()
+    {
+	_plugin = CppPlugin.getDefault();
+    }
+
+    public void run(IAction action)     
+    {
+	// add C++ project indicator
+	IPath newPath = _project.getFullPath();
+	QualifiedName indicatorFile = new QualifiedName("C++ Project", newPath.toString());
+	try
+	    {
+		_project.setPersistentProperty(indicatorFile, "yes");
+	    }
+	catch (CoreException ce)
+	    {
+	    }
+		
+	// add parse paths
+	ArrayList paths = _plugin.readProperty("DefaultParseIncludePath");
+	_plugin.writeProperty(_project, "Include Path", paths);
+	
+	// add parse behaviour
+	ArrayList autoParse = _plugin.readProperty("AutoParse");
+	_plugin.writeProperty(_project, "AutoParse", autoParse);      
+
+	ArrayList autoPersist = _plugin.readProperty("AutoPerist");
+	_plugin.writeProperty(_project, "AutoPersist", autoPersist);      
+
+	// add parse quality
+	ArrayList preferences = _plugin.readProperty("ParseQuality");
+	_plugin.writeProperty(_project, "ParseQuality", preferences);      
+		
+	// add build history
+	ArrayList builds = _plugin.readProperty("DefaultBuildInvocation");
+	_plugin.writeProperty(_project, "Build History", builds);
+
+	// add clean history
+	ArrayList cleans = _plugin.readProperty("DefaultCleanInvocation");
+	_plugin.writeProperty(_project, "Clean History", cleans);
+
+	ArrayList variables = _plugin.readProperty("DefaultEnvironment");
+	_plugin.writeProperty(_project, "Environment", variables);
+
+	ModelInterface api = ModelInterface.getInstance();
+
+	// add build spec
+	try 
+	    { 
+		// specify nature
+		IProjectDescription projectDescription =  _project.getDescription();
+		String[] natures = projectDescription.getNatureIds();
+		String[] newNatures = new String[natures.length + 1];
+		System.arraycopy(natures, 0, newNatures, 0, natures.length);
+		newNatures[natures.length] = "com.ibm.cpp.ui.cppnature";
+		projectDescription.setNatureIds(newNatures);
+		_project.setDescription(projectDescription, null);
+	    } 
+	catch (CoreException e)  
+	    {
+		System.out.println(e);
+	    }
+    }
+    
+    
+    public void selectionChanged(IAction action, ISelection selection) 
+    {
+	if (selection instanceof IStructuredSelection)
+        {
+	    IStructuredSelection structuredSelection= (IStructuredSelection)selection;
+	    IResource resource = (IResource)structuredSelection.getFirstElement();
+	    if (resource instanceof IProject)
+		{
+		    if (!_plugin.isCppProject((IProject)resource))
+			{
+			    _project = (IProject)resource;
+			    ((Action)action).setEnabled(true);
+			    return;
+			}
+		}
+	}
+	((Action)action).setEnabled(false);
+    }
+    
+    public void selectionChanged(SelectionChangedEvent selection) 
+    {
+    }
+}
