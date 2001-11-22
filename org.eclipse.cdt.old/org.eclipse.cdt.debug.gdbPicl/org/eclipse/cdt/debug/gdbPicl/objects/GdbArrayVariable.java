bec52fa54882ceebea15b92803d139abc4778ae8
Samantha Chan <chanskw@ca.ibm.com>
2001-11-22 17:00:51 +0000

    Added copyright

--- org.eclipse.cdt.old/org.eclipse.cdt.debug.gdbPicl/org/eclipse/cdt/debug/gdbPicl/objects/GdbArrayVariable.java
+++ org.eclipse.cdt.old/org.eclipse.cdt.debug.gdbPicl/org/eclipse/cdt/debug/gdbPicl/objects/GdbArrayVariable.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 1995, 2001 International Business Machines Corporation. All rights reserved.
+ * This program and the accompanying materials are made available under the terms of
+ * the Common Public License which accompanies this distribution.
+ */
+
 package com.ibm.debug.gdbPicl.objects;
 
 import com.ibm.debug.epdc.EStdTreeNode;
