1941fc8af4c32f92a209b29571fe5e21806c2032
Jeff Turnham <turnham@ca.ibm.com>
2001-11-28 14:41:59 +0000

    *** empty log message ***

---
+++ org.eclipse.cdt.old/org.eclipse.cdt.dstore.core/build/GenerateMakefiles.java
@@ -0,0 +1,188 @@
+
+/*
+ * Copyright (c) 2001 International Business Machines Corporation. All rights reserved.
+ * This program and the accompanying materials are made available under the terms of
+ * the Common Public License which accompanies this distribution.
+ */
+
+import java.io.*;
+import java.util.*;
+
+class GenerateMakefiles
+{
+ private static String _pluginsDir;
+ private static String _theRulesMakefile;
+ 
+ public static void main(String args[])
+ {
+  _pluginsDir = getPluginsDirectory();
+  if (args.length == 0)
+  {
+   System.out.println(_pluginsDir);
+   return;
+  }
+  _theRulesMakefile = _pluginsDir + "/com.ibm.dstore.core/rules.mk";
+  for (int i=0; i<args.length; i++)
+   generateBuildMakefilesFor(args[i]);
+ }
+ 
+ 
+ private static void generateBuildMakefilesFor(String thePlugin)
+ {
+  String envMakefile = generateEnvironmentMakefile(thePlugin);
+  File srcDirectory = new File (_pluginsDir + "/" + thePlugin);
+  if (srcDirectory.exists())
+   recursiveCreateMakefiles(srcDirectory, envMakefile);
+ }
+
+ private static void recursiveCreateMakefiles(File theDirectory, String theEnvMakefile)
+ {
+  String theMakefile = theDirectory + "/makefile";
+  String fileContents = "include " + theEnvMakefile + "\ninclude " + _theRulesMakefile;
+  writeFile(theMakefile, fileContents);
+ 
+  File[] subdirs = theDirectory.listFiles();
+  for (int i=0; i<subdirs.length; i++)
+  {
+   File theSubdir = subdirs[i];
+   if ( theSubdir.isDirectory() && (!theSubdir.getName().equals("CVS")) && (!theSubdir.getName().equals("build")))
+    recursiveCreateMakefiles(theSubdir, theEnvMakefile);
+  }
+ }
+ 
+ private static String generateEnvironmentMakefile(String thePlugin)
+ {
+  String setenvMakefile = _pluginsDir + "/" + thePlugin + "/setenv.mk";
+  writeFile(setenvMakefile, getEnvironmentInfo(thePlugin));
+  return setenvMakefile;
+ }
+
+ private static void writeFile(String fileName, String contents)
+ {
+  try
+  {
+   File theFile = new File (fileName);
+   if (theFile.exists())
+    theFile.delete();
+   theFile.createNewFile();
+   PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(theFile)));
+   out.println("# Copyright (c) 2001 International Business Machines Corporation. All rights reserved.");
+   out.println("# This program and the accompanying materials are made available under the terms of the");
+   out.println "# Common Public License which accompanies this distribution.");
+   out.println("SHELL=sh");
+   out.println(contents);
+   out.flush();
+   out.close();
+  }
+  catch (IOException e) {}
+ }
+ 
+ /* Very specific method to generate a jar file name...
+  * Currently we just take the last 2 parts of the plugin name and separate them by a _,
+  * so com.ibm.dstore.core becomes dstore_core.jar
+  */
+ private static String getJarFileName(String thePlugin)
+ {
+  if (thePlugin.indexOf("extra") >= 0)
+   thePlugin = thePlugin + "_client";
+  int firstDot  = thePlugin.indexOf(".");
+  int secondDot = thePlugin.indexOf(".",firstDot+1);
+  return thePlugin.substring(secondDot+1, thePlugin.length()).replace('.','_') + ".jar";
+ }
+ 
+ private static String getEnvironmentInfo(String thePlugin)
+ {
+  StringBuffer env = new StringBuffer();
+  env.append("pluginsDirectory:=" + _pluginsDir +"\n");
+  env.append("pluginName:=" + thePlugin +"\n");
+  env.append("jarFile:=" + getJarFileName(thePlugin) +"\n");
+
+  File pluginXML = new File(_pluginsDir + "/" + thePlugin + "/plugin.xml");
+  if (!pluginXML.exists())
+   pluginXML = new File(_pluginsDir + "/" + thePlugin + "/Imports.make");
+  
+  ArrayList classpaths = getClassPaths(pluginXML);
+  env.append("cp:=" + _pluginsDir + "/" + thePlugin + "\\\n");
+  for (int i=0; i<classpaths.size(); i++)
+  {
+   ArrayList theJars = getJars((String)classpaths.get(i));
+   for (int j=0; j<theJars.size(); j++)
+    env.append(((String)theJars.get(j))+"\\\n");
+  }
+  env.append("\n\n");
+  return env.toString();
+ }
+ 
+ private static ArrayList getClassPaths(File pluginXML)
+ {
+  ArrayList theClassPaths = new ArrayList();
+  try
+  {
+   BufferedReader br = new BufferedReader(new FileReader(pluginXML));
+   String nextLine;
+   while ( (nextLine = br.readLine()) != null)
+   {
+    if (nextLine.indexOf("import plugin=") > -1)
+    {
+     int firstQuote = nextLine.indexOf("\"");
+     if (firstQuote > -1)
+     {
+      int secondQuote = nextLine.indexOf("\"", firstQuote+1);
+      if (secondQuote > firstQuote)
+       theClassPaths.add(nextLine.substring(firstQuote+1, secondQuote));
+     }
+    }
+   }
+  }
+  catch (IOException e) { }
+  return theClassPaths;
+ }
+ 
+ private static ArrayList getJars(String theClassPath)
+ {
+  ArrayList jarNames = new ArrayList();
+  try
+  {
+   File pluginXML = new File (_pluginsDir + "/" + theClassPath + "/plugin.xml");
+   BufferedReader br = new BufferedReader(new FileReader(pluginXML));
+   String nextLine;
+   while ( (nextLine = br.readLine()) != null)
+   {
+    if (nextLine.indexOf("library name=") > -1)
+    {
+     int firstQuote = nextLine.indexOf("\"");
+     if (firstQuote > -1)
+     {
+      int secondQuote = nextLine.indexOf("\"", firstQuote+1);
+      if (secondQuote > firstQuote)
+      { 
+       String theJar = nextLine.substring(firstQuote+1, secondQuote);
+       if (theJar.equals("."))
+        jarNames.add(_pluginsDir + "/" + theClassPath);
+       else
+        jarNames.add(_pluginsDir + "/" + theClassPath + "/" + theJar);        
+      }
+     }      
+    }
+   }
+  }
+  catch (IOException e) {}
+  return jarNames;
+ }
+ 
+ private static String getPluginsDirectory()
+ {
+  String plugins = new String("plugins");
+  String pwd = System.getProperty("user.dir");
+  try
+  { 
+   pwd = new File(pwd).getCanonicalPath();
+  }
+  catch (IOException e) {}
+  return pwd.substring(0, pwd.indexOf(plugins) + plugins.length()).replace('\\','/');
+ } 
+}
+
+ 
+
+
---
+++ org.eclipse.cdt.old/org.eclipse.cdt.dstore.core/build/rules.mk
@@ -0,0 +1,79 @@
+# 
+# Copyright (c) 2001 International Business Machines Corporation. All rights reserved.
+# This program and the accompanying materials are made available under the terms of
+# the Common Public License which accompanies this distribution.
+#
+SHELL=sh
+
+# First We Set Up Some Helper Variables
+# ----------------
+#
+# Grab the .java files from the current directory and replace the .java with .class
+define get-files
+ $(patsubst %.java, %.class, $(wildcard *.java))
+endef
+
+# Grab the directories from the current directory, weed out CVS, and append _dir to each
+define get-directories
+ $(patsubst %, %_dir, $(filter-out build, $(filter-out %extra.Client, $(filter-out %extra.Server, $(filter-out %CVS, $(shell find * -type d -maxdepth 0))))))
+endef
+
+# Determine the system path separator
+ifeq (win, $(findstring win,$(subst W,w,$(OS))))
+ sep:= \;
+else
+ sep:= :
+endif
+
+# These are used to help convert the variable cp to a path-separated Classpath
+empty:=
+space:= $(empty) $(empty)
+
+
+# Public Targets
+# --------------
+#
+all               : $(get-directories) $(get-files)
+jar               : createJarFile 
+source-zip        : createSourceZip
+clean             : doClean
+clean-makefiles   : doCleanMakefiles
+
+
+# Private\Implicit Targets
+# ------------------------
+#
+#addCopyrights:=$(patsubst %, java -cp f:/programs/cygwin/usr/local/javautils/addCopyright.jar addCopyright % "Copyright (c) 2001 International Business Machines Corporation. All rights reserved. This program and the accompanying materials are made available under the terms of the Common Public License which accompanies this distribution.";,$(shell find $(pluginsDirectory)/$(pluginName) -type d))
+
+%.class: %.java
+	@echo " Compiling" $(patsubst %.class, %.java, $@)
+	@javac -classpath $(subst $(space),$(sep),$(cp)) $(JAVACFLAGS) $<
+
+%_dir:
+	@$(MAKE) -C $(patsubst %_dir,%, $@)
+
+createJarFile:
+	@cd $(pluginsDirectory)/$(pluginName);\
+	rm -f $(jarFile);\
+	touch $(jarFile);\
+	find . -name '*.class' -or -name '*.properties' | xargs jar -uf $(jarFile)
+	@echo " Created" $(pluginsDirectory)/$(pluginName)/$(jarFile) 
+
+createSourceZip:
+	@cd $(pluginsDirectory)/$(pluginName);\
+	zip -qr $(patsubst %.jar,%src.zip,$(jarFile)) . -i *.java *.jj *build/makefile *rules.mk
+	@echo " Created" $(pluginsDirectory)/$(pluginName)/ $(patsubst %.jar,%src.zip,$(jarFile)) 
+
+doClean:
+	@cd $(pluginsDirectory)/$(pluginName);\
+	find . -name '*.class' |xargs rm -f
+
+
+
+
+
+
+
+
+
+
