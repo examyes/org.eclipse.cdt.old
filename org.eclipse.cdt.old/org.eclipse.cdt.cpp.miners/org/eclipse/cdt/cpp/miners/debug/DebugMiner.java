232eda3f1b25a4bd9f4c8075b44d62cdb0dc41fa
Jeff Turnham <turnham@ca.ibm.com>
2001-12-03 21:26:55 +0000

    *** empty log message ***

--- org.eclipse.cdt.old/org.eclipse.cdt.cpp.miners/org/eclipse/cdt/cpp/miners/debug/DebugMiner.java
+++ org.eclipse.cdt.old/org.eclipse.cdt.cpp.miners/org/eclipse/cdt/cpp/miners/debug/DebugMiner.java
@@ -40,7 +40,8 @@ public class DebugMiner extends Miner
 	String debugPath = _dataStore.getAttribute(DataStoreAttributes.A_PLUGIN_PATH) + "com.ibm.debug";
 	String ps = System.getProperty("path.separator");
 	String fs = "/";
-	_debugJarPath =       debugPath + fs + "ibm_debug.jar";
+        _debugJarPath =  debugPath;
+	_debugJarPath += ps + debugPath + fs + "ibm_debug.jar";
 	_debugJarPath += ps + _gdbPiclPath;
 	_debugInvocation = "java -cp " + _debugJarPath + " " + _debugOptions + " com.ibm.debug.gdbPicl.Gdb ";
     }
