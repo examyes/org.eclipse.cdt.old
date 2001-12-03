8a2e90099a501618b1b3e59c9e8de5d61fef9464
Enrique Varillas <enriquev>
2001-12-03 18:32:33 +0000

    Added icons to table entries

--- org.eclipse.cdt.old/org.eclipse.cdt.linux.help/org/eclipse/cdt/linux/help/views/ResultsViewPart.java
+++ org.eclipse.cdt.old/org.eclipse.cdt.linux.help/org/eclipse/cdt/linux/help/views/ResultsViewPart.java
@@ -343,16 +343,19 @@ public class ResultsViewPart extends ViewPart
 		
 		if (type == ItemElement.MAN_TYPE)
 		    {
+			ti.setImage(0,_plugin.getImage("man.gif"));
        			ti.setText(0,manType);
 			ti.setText(1,theItem.getName());			
 		    }
 		else if (type == ItemElement.INFO_TYPE)
-		    {						
+		    {	
+			ti.setImage(0,_plugin.getImage("info.gif"));
 			ti.setText(0,infoType);
 			ti.setText(1,theItem.getKey());
 		    }
 		else if (type == ItemElement.HTML_TYPE)
 		    {
+			ti.setImage(0,_plugin.getImage("html.gif"));
 			ti.setText(0,htmlType);
 			String filename = theItem.getName();
 			ti.setText(1,filename.substring(filename.lastIndexOf(File.separator)+1));
@@ -380,6 +383,8 @@ public class ResultsViewPart extends ViewPart
     public void dispose()
     {
 	super.dispose();
+	if(_expressionColor!=null)
+	    _expressionColor.dispose();
 	if(_tablecolor!=null)
 	    _tablecolor.dispose();
 	if (_table!=null)
