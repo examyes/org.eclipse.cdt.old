94ea772661a0115f233b17c6a69bdaf3a47faff5
Jeff Turnham <turnham@ca.ibm.com>
2001-12-04 23:22:45 +0000

    *** empty log message ***

--- org.eclipse.cdt.old/org.eclipse.cdt.dstore.core/win.env.bat
+++ org.eclipse.cdt.old/org.eclipse.cdt.dstore.core/win.env.bat
@@ -1,7 +1,7 @@
 set ECLIPSEDIR=e:\eclipse\eclipse
 set CORE=%ECLIPSEDIR%\plugins\com.ibm.dstore.core
 set MINERS=%ECLIPSEDIR%\plugins\com.ibm.dstore.miners
-set EXTRA=%ECLIPSEDIR%\plugins\com.ibm.dstore.extra\dstore_extra_server.jar
+set EXTRA=%ECLIPSEDIR%\plugins\com.ibm.dstore.extra.server\extra_server.jar
 set CPP_MINERS=%ECLIPSEDIR%\plugins\com.ibm.cpp.miners
 set PARSER_MINER=%ECLIPSEDIR%\plugins\com.ibm.cpp.miners.parser
 
