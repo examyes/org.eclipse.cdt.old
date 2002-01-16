7f2c7acd4f4fe3d17f65ad248c627089a6ebefbd
Jeff Turnham <turnham@ca.ibm.com>
2002-01-16 16:59:51 +0000

    Fixed syntax error

--- org.eclipse.cdt.old/org.eclipse.cdt.cpp.miners.parser/org/eclipse/cdt/cpp/miners/parser/ParseMiner.java
+++ org.eclipse.cdt.old/org.eclipse.cdt.cpp.miners.parser/org/eclipse/cdt/cpp/miners/parser/ParseMiner.java
@@ -503,7 +503,7 @@ public class ParseMiner extends Miner
  private void handleObjectParse(DataElement theElement, DataElement status)
  {
   StringBuffer theBuf = theElement.getBuffer();
-  if ((theElement.getType().equals(ParserSchema.ParsedSource) || (theBuf == null) || (theBuf.length() == 0))
+  if ((theElement.getType().equals(ParserSchema.ParsedSource)) || (theBuf == null) || (theBuf.length() == 0))
    return;
   _parseManager.parseObject(theElement, status);
  }
