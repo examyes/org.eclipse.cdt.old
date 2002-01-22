981ae046af1813c5b92e14198d2f7f01086cc78c
David McKnight <dmcknigh@ca.ibm.com>
2002-01-22 23:02:13 +0000

    javadoc

--- org.eclipse.cdt.old/org.eclipse.cdt.dstore.core/org/eclipse/cdt/dstore/core/model/DE.java
+++ org.eclipse.cdt.old/org.eclipse.cdt.dstore.core/org/eclipse/cdt/dstore/core/model/DE.java
@@ -7,50 +7,195 @@ package org.eclipse.cdt.dstore.core.model;
  */
 
 
-// constants for dataelements
+/**
+ * DE is a container of <code>DataElement</code> constants.  These constants
+ * are used to identify <code>DataElement</code> attributes.
+ * 
+ * <li>
+ * Attributes beginning with "P_" indicate <I>property</I> attribute identifiers.
+ * </li>
+ * <li>
+ * Attributes beginning with "T_" indicate <code>DataElement</code> <I>type</I> attributes.
+ * </li>
+ * <li>
+ * Attributes beginning with "A_" indicate <code>DataElement</code> indexs into <I>attributes</I>.
+ * </li>
+ *
+ */
 public class DE 	     
 {
-  public static final String P_CHILDREN = "children";
-  public static final String P_IMAGE = "org.eclipse.jface.icon";
-  public static final String P_LABEL = "label";
-  public static final String P_NOTIFIER = "notifier";
+    /*
+     * The nested data (children) property identifier of a <code>DataElement</code>.
+     */
+    public static final String P_CHILDREN = "children";
 
-  public static final String P_DATASTORE       = "dataStore";
-  public static final String P_SOURCE_NAME     = "source";
-  public static final String P_SOURCE          = "sourcefile";
-  public static final String P_SOURCE_LOCATION = "sourcelocation";
-  public static final String P_SOURCE_LOCATION_COLUMN = "sourcelocationcolumn";
-  public static final String P_NESTED          = "nested";
-  public static final String P_BUFFER          = "buffer";
-  public static final String P_TYPE            = "type";
-  public static final String P_ID              = "id";
-  public static final String P_NAME            = "name";
-  public static final String P_VALUE           = "value";
-  public static final String P_ISREF           = "isRef";
-  public static final String P_DEPTH           = "depth";
-  public static final String P_ATTRIBUTES      = "attribute";
-  public static final String P_FILE            = "file";
+    /*
+     * The image property identifier of a <code>DataElement</code>.  This is the same
+     * as the value property identifier
+     */
+    public static final String P_LABEL = "label";
 
-  // a reference to another element
-  public static final String T_REFERENCE          = "reference";
-  public static final String T_COMMAND            = "command";
-  public static final String T_UI_COMMAND_DESCRIPTOR  = "ui_commanddescriptor";
+    /*
+     * The notifier property identifier of a <code>DataElement</code>.
+     */
+    public static final String P_NOTIFIER = "notifier";
 
-  public static final String T_OBJECT_DESCRIPTOR  = "objectdescriptor";
-  public static final String T_COMMAND_DESCRIPTOR = "commanddescriptor";
-  public static final String T_RELATION_DESCRIPTOR = "relationdescriptor";
+    /*
+     * The <code>DataStore</code> property identifier of a <code>DataElement</code>.
+     */
+    public static final String P_DATASTORE       = "dataStore";
 
-  public static final String T_ABSTRACT_OBJECT_DESCRIPTOR = "abstractobjectdescriptor";
-  public static final String T_ABSTRACT_COMMAND_DESCRIPTOR = "abstractcommanddescriptor";
-  public static final String T_ABSTRACT_RELATION_DESCRIPTOR = "abstractrelationdescriptor";
+    /*
+     * The source name property identifier of a <code>DataElement</code>.  This is the
+     * name of a source location if one exists.
+     */
+    public static final String P_SOURCE_NAME     = "source";
 
-  // indexes of element attributes
-  public static final int    A_TYPE       = 0;
-  public static final int    A_ID         = 1;
-  public static final int    A_NAME       = 2;
-  public static final int    A_VALUE      = 3;
-  public static final int    A_SOURCE     = 4;
-  public static final int    A_ISREF      = 5;
-  public static final int    A_DEPTH      = 6;
-  public static final int    A_SIZE       = 7;
+    /*
+     * The source file property identifier of a <code>DataElement</code>.
+     */
+    public static final String P_SOURCE          = "sourcefile";
+
+    /*
+     * The source location property identifier of a <code>DataElement</code>.
+     */
+    public static final String P_SOURCE_LOCATION = "sourcelocation";
+
+    public static final String P_SOURCE_LOCATION_COLUMN = "sourcelocationcolumn";
+
+    /*
+     * The nested data (children) property identifier of a <code>DataElement</code>.  Same as <code>P_CHILDREN</code>.
+     */
+    public static final String P_NESTED          = "nested";
+
+    /*
+     * The buffer property identifier of a <code>DataElement</code>.  
+     */
+    public static final String P_BUFFER          = "buffer";
+
+    /*
+     * The type property identifier of a <code>DataElement</code>.  
+     */
+    public static final String P_TYPE            = "type";
+
+    /*
+     * The id property identifier of a <code>DataElement</code>.  
+     */
+    public static final String P_ID              = "id";
+
+    /*
+     * The name property identifier of a <code>DataElement</code>.  
+     */
+    public static final String P_NAME            = "name";
+
+    /*
+     * The value property identifier of a <code>DataElement</code>.  
+     */
+    public static final String P_VALUE           = "value";
+
+    /*
+     * The <I>is reference?</I> property identifier of a <code>DataElement</code>.  
+     */
+    public static final String P_ISREF           = "isRef";
+
+    /*
+     * The visibility property identifier of a <code>DataElement</code>.  
+     */
+    public static final String P_DEPTH           = "depth";
+
+    /*
+     * The attributes property identifier of a <code>DataElement</code>.  
+     */
+    public static final String P_ATTRIBUTES      = "attribute";
+
+    /*
+     * The file property identifier of a <code>DataElement</code>.  
+     */
+    public static final String P_FILE            = "file";
+
+    /*
+     * Reference type.  
+     */
+    public static final String T_REFERENCE          = "reference";
+
+    /*
+     * Command type.  
+     */
+    public static final String T_COMMAND            = "command";
+
+    /*
+     * UI Command Descriptor type.  
+     */
+    public static final String T_UI_COMMAND_DESCRIPTOR  = "ui_commanddescriptor";
+
+    /*
+     * Object Descriptor type.  
+     */
+    public static final String T_OBJECT_DESCRIPTOR  = "objectdescriptor";
+
+    /*
+     * Command Descriptor type.  
+     */
+    public static final String T_COMMAND_DESCRIPTOR = "commanddescriptor";
+
+    /*
+     * Relation Descriptor type.  
+     */
+    public static final String T_RELATION_DESCRIPTOR = "relationdescriptor";
+
+    /*
+     * Abstract Object Descriptor type.  
+     */
+    public static final String T_ABSTRACT_OBJECT_DESCRIPTOR = "abstractobjectdescriptor";
+
+    /*
+     * Abstract Command Descriptor type.  
+     */
+    public static final String T_ABSTRACT_COMMAND_DESCRIPTOR = "abstractcommanddescriptor";
+
+    /*
+     * Abstract Relation Descriptor type.  
+     */
+    public static final String T_ABSTRACT_RELATION_DESCRIPTOR = "abstractrelationdescriptor";
+
+
+    /*
+     * Type attribute index.  
+     */
+    public static final int    A_TYPE       = 0;
+
+    /*
+     * ID attribute index.  
+     */
+    public static final int    A_ID         = 1;
+
+    /*
+     * Name attribute index.  
+     */
+    public static final int    A_NAME       = 2;
+
+    /*
+     * Value attribute index.  
+     */
+    public static final int    A_VALUE      = 3;
+
+    /*
+     * Source attribute index.  
+     */
+    public static final int    A_SOURCE     = 4;
+
+    /*
+     * IsRef attribute index.  
+     */
+    public static final int    A_ISREF      = 5;
+
+    /*
+     * Visibility attribute index.  
+     */
+    public static final int    A_DEPTH      = 6;
+
+    /*
+     * Size attribute index.  
+     */
+    public static final int    A_SIZE       = 7;
 }
