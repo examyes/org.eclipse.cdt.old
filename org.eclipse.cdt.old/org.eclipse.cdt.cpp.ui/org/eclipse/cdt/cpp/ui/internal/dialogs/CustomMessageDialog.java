d9521aa8d9305f117e7fb13e754845355c9e8f0a
Yasser Elmankabady <eyasser@ca.ibm.com>
2002-01-26 19:36:22 +0000

    copy right

--- org.eclipse.cdt.old/org.eclipse.cdt.cpp.ui/org/eclipse/cdt/cpp/ui/internal/dialogs/CustomMessageDialog.java
+++ org.eclipse.cdt.old/org.eclipse.cdt.cpp.ui/org/eclipse/cdt/cpp/ui/internal/dialogs/CustomMessageDialog.java
@@ -1,4 +1,9 @@
 package org.eclipse.cdt.cpp.ui.internal.dialogs;
+/*
+ * Copyright (c) 2001 International Business Machines Corporation. All rights reserved.
+ * This program and the accompanying materials are made available under the terms of
+ * the Common Public License which accompanies this distribution.
+ */
 
 import java.util.ArrayList;
 import org.eclipse.cdt.cpp.ui.internal.actions.ConfigureAction;
