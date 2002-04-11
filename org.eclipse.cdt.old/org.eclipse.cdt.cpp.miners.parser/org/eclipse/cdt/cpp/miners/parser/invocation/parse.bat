fc4b0ac316fc49573856f6362010c4008314cb8b
Jeff Turnham <turnham@ca.ibm.com>
2002-04-11 20:44:26 +0000

    Removing parse.bat since command line parsing is going to be supported by actually creating a DataStore

--- org.eclipse.cdt.old/org.eclipse.cdt.cpp.miners.parser/org/eclipse/cdt/cpp/miners/parser/invocation/parse.bat
+++
@@ -1,6 +0,0 @@
-#Copyright (C) 2000, 2001, 2002 International Business Machines Corporation and others. All Rights Reserved.  
-@echo off
-set CLASSPATHBACKUP=%CLASSPATH%
-set CLASSPATH=e:\eclipse\plugins\org.eclipse.cdt.cpp.miners;%CLASSPATH%
-java org.eclipse.cdt.cpp.miners.parser.invocation.CommandLine %1
-set CLASSPATH=%CLASSPATHBACKUP%
