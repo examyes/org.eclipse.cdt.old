12b1bf6ebc877f63c5362f1bec922bd063132c7a
David McKnight <dmcknigh@ca.ibm.com>
2002-04-12 14:28:46 +0000

    new browse actions

---
+++ org.eclipse.cdt.old/org.eclipse.cdt.cpp.ui/org/eclipse/cdt/cpp/ui/internal/actions/BrowseObjectAction.java
@@ -0,0 +1,89 @@
+package org.eclipse.cdt.cpp.ui.internal.actions;
+
+/*
+ * Copyright (c) 2001, 2002 International Business Machines Corporation. All rights reserved.
+ * This program and the accompanying materials are made available under the terms of
+ * the Common Public License which accompanies this distribution.
+ */
+
+import org.eclipse.cdt.cpp.ui.internal.*;
+import org.eclipse.cdt.cpp.ui.internal.views.*;
+
+import org.eclipse.cdt.dstore.core.model.*;
+import org.eclipse.cdt.dstore.ui.*;
+import org.eclipse.cdt.dstore.ui.views.*;
+import org.eclipse.cdt.dstore.ui.actions.*;
+
+import org.eclipse.core.resources.*;
+import org.eclipse.core.runtime.*;
+
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.ui.*;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.core.internal.resources.*;
+
+import java.util.*;
+import java.lang.reflect.*;
+
+import org.eclipse.jface.action.*;
+import org.eclipse.ui.internal.*;
+
+public class BrowseObjectAction extends Action 
+{
+    private DataElement _subject;
+    private String      _viewId;
+    private DataElement _relationship;
+
+    public BrowseObjectAction(DataElement relationship, DataElement subject, String viewId)
+    {	
+        super(relationship.getName());
+	_relationship = relationship;
+	_subject = subject;
+	_viewId = viewId;
+    }
+
+    public void run() 
+    {
+	if (_subject != null)
+	    {
+		openView();
+	    }
+    }
+    
+
+    public void openView()
+    {
+      IWorkbench desktop = WorkbenchPlugin.getDefault().getWorkbench();
+      IWorkbenchWindow win = desktop.getActiveWorkbenchWindow();
+
+      IWorkbenchPage persp= win.getActivePage();
+      ILinkable viewPart = (ILinkable)persp.findView(_viewId);
+
+      if (viewPart == null)
+	  {
+	      try
+		  {
+		      persp.showView(_viewId);
+		      viewPart = (ILinkable)persp.findView(_viewId);
+								
+		  }
+	      catch (PartInitException e)
+		  {
+		      System.out.println(e);
+		  }
+	  }
+      if (viewPart != null)
+	  {
+	      viewPart.setInput(_subject);		
+	      if (viewPart instanceof GenericViewPart)
+		  {
+		      GenericViewPart gvp = (GenericViewPart)viewPart;
+		      gvp.fixateOnRelationType(_relationship.getName());			  
+		  }
+	  }
+
+    }
+
+
+}
---
+++ org.eclipse.cdt.old/org.eclipse.cdt.cpp.ui/org/eclipse/cdt/cpp/ui/internal/actions/BrowseProjectAction.java
@@ -0,0 +1,73 @@
+package org.eclipse.cdt.cpp.ui.internal.actions;
+
+/*
+ * Copyright (c) 2001, 2002 International Business Machines Corporation. All rights reserved.
+ * This program and the accompanying materials are made available under the terms of
+ * the Common Public License which accompanies this distribution.
+ */
+
+import org.eclipse.cdt.cpp.ui.internal.*;
+import org.eclipse.cdt.cpp.ui.internal.views.*;
+
+import org.eclipse.cdt.dstore.core.model.*;
+import org.eclipse.cdt.dstore.ui.*;
+import org.eclipse.cdt.dstore.ui.views.*;
+import org.eclipse.cdt.dstore.ui.actions.*;
+
+import org.eclipse.core.resources.*;
+import org.eclipse.core.runtime.*;
+
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.ui.*;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.core.internal.resources.*;
+
+import java.util.*;
+import java.lang.reflect.*;
+
+import org.eclipse.jface.action.*;
+import org.eclipse.ui.internal.*;
+
+public class BrowseProjectAction extends Action 
+{
+    private DataElement _subject;
+    private String      _perspective;
+
+    public BrowseProjectAction(String label, DataElement subject)
+    {	
+        super(label);
+	_subject = subject;
+	_perspective = "org.eclipse.cdt.cpp.ui.CppBrowsePerspective";
+    }
+
+    public BrowseProjectAction(String label, DataElement subject, String perspective)
+    {	
+        super(label);
+	_subject = subject;
+	_perspective = perspective;
+    }
+    
+    public void run() 
+    {
+	if (_subject != null)
+	    {
+		openPerspective(_subject);
+	    }
+    }
+    
+    private void openPerspective(DataElement input)
+    {
+        IWorkbench workbench = CppPlugin.getDefault().getWorkbench();
+	IWorkbenchPage persp = null;
+	
+	try 
+	    {
+		persp = workbench.openPage(_perspective, input, 0);
+	    }
+        catch (WorkbenchException e)
+	    {
+	    }		    
+    }
+
+}
