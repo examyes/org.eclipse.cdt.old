a4dfb56c18fc8e10b783253edc4f017141780e7f
Morris Kwan <mkwan@ca.ibm.com>
2002-04-17 15:21:58 +0000

    add file PACallArc.java

---
+++ org.eclipse.cdt.old/org.eclipse.cdt.cpp.miners/org/eclipse/cdt/cpp/miners/pa/engine/PACallArc.java
@@ -0,0 +1,71 @@
+package org.eclipse.cdt.cpp.miners.pa.engine;
+
+/**
+ * PACallArc represents a call arc in the call graph.
+ * The call arc contains information about which caller calls which callee, the
+ * number of calls and time information.
+ */
+public class PACallArc {
+
+  private PATraceFunction _caller;
+  private PATraceFunction _callee;
+  private int _callNumber;
+  private double _selfTime;
+  private double _childrenTime;
+  
+  // constructor
+  public PACallArc(PATraceFunction caller, PATraceFunction callee) {
+   _caller = caller;
+   _callee = callee;
+  }
+
+  public PACallArc(PATraceFunction caller, PATraceFunction callee, int callNumber, double selfTime, double childrenTime) {
+   _caller = caller;
+   _callee = callee;
+   _callNumber = callNumber;
+   _selfTime = selfTime;
+   _childrenTime = childrenTime;
+  }
+  
+  // setter methods
+  public void setCallNumber(int callNumber) {
+   _callNumber = callNumber;
+  }
+  
+  public void setSelfTime(double selfTime) {
+   _selfTime = selfTime;
+  }
+  
+  public void setChildrenTime(double childrenTime) {
+   _childrenTime = childrenTime;
+  }
+  
+  public void setCaller(PATraceFunction caller) {
+   _caller = caller;
+  }
+  
+  public void setCallee(PATraceFunction callee) {
+   _callee = callee;
+  }
+  
+  // getter methods
+  public PATraceFunction getCaller() {
+   return _caller;
+  }
+
+  public PATraceFunction getCallee() {
+   return _callee;
+  }
+
+  public int getCallNumber() {
+   return _callNumber;
+  }
+  
+  public double getSelfTime() {
+   return _selfTime;
+  }
+
+  public double getChildrenTime() {
+   return _childrenTime;
+  } 
+}
