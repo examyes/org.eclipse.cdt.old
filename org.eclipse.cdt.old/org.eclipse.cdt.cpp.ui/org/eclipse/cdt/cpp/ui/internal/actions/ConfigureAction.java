8455196c8e9c5dd8f7164e4bfe193d56d1488247
Yasser Elmankabady <eyasser@ca.ibm.com>
2002-04-24 14:21:11 +0000

    organize

--- org.eclipse.cdt.old/org.eclipse.cdt.cpp.ui/org/eclipse/cdt/cpp/ui/internal/actions/ConfigureAction.java
+++ org.eclipse.cdt.old/org.eclipse.cdt.cpp.ui/org/eclipse/cdt/cpp/ui/internal/actions/ConfigureAction.java
@@ -6,41 +6,21 @@ package org.eclipse.cdt.cpp.ui.internal.actions;
  * the Common Public License which accompanies this distribution.
  */
 
-import org.eclipse.cdt.cpp.ui.internal.api.*;
-import org.eclipse.cdt.cpp.ui.internal.dialogs.CustomMessageDialog;
-import org.eclipse.cdt.cpp.ui.internal.dialogs.PreventableMessageBox;
-import org.eclipse.cdt.cpp.ui.internal.*;
+import java.util.ArrayList;
+
 import org.eclipse.cdt.cpp.ui.internal.CppPlugin;
-
-import org.eclipse.cdt.dstore.ui.actions.*;
-import org.eclipse.cdt.dstore.core.model.*;
-
-import java.io.*; 
-import java.util.*;
-
-import org.eclipse.jface.action.*;
-
-import org.eclipse.core.runtime.*;
-import org.eclipse.core.resources.IMarker;
+import org.eclipse.cdt.cpp.ui.internal.api.ModelInterface;
+import org.eclipse.cdt.cpp.ui.internal.dialogs.CustomMessageDialog;
+import org.eclipse.cdt.dstore.core.model.DataElement;
+import org.eclipse.cdt.dstore.core.model.DataStore;
+import org.eclipse.cdt.dstore.core.model.Handler;
+import org.eclipse.cdt.dstore.ui.actions.CustomAction;
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IResource;
-import org.eclipse.core.resources.IWorkspaceRoot;
-import org.eclipse.core.resources.IWorkspaceRunnable;
-//import org.eclipse.core.resources.*;
-import org.eclipse.core.runtime.CoreException;
-
-import org.eclipse.ui.*;
-import org.eclipse.ui.internal.*;
-import org.eclipse.ui.IEditorInput;
-import org.eclipse.ui.IFileEditorInput;
-import org.eclipse.ui.texteditor.AbstractMarkerAnnotationModel;
-import org.eclipse.ui.texteditor.ITextEditor;
-import org.eclipse.ui.texteditor.MarkerRulerAction;
-
+import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
-import org.eclipse.swt.widgets.*;
-import org.eclipse.jface.dialogs.*;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Widget;
 
 
 public class ConfigureAction extends CustomAction implements SelectionListener
