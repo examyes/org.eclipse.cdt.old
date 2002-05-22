8c1325cb80a7b45ec572f1ab4230fef515cdaf08
Enrique Varillas <enriquev>
2002-05-22 20:09:50 +0000

    initial remote help support

--- org.eclipse.cdt.old/org.eclipse.cdt.linux.help/org/eclipse/cdt/linux/help/actions/HelpPreferencesActionDelegate.java
+++ org.eclipse.cdt.old/org.eclipse.cdt.linux.help/org/eclipse/cdt/linux/help/actions/HelpPreferencesActionDelegate.java
@@ -13,25 +13,25 @@ import org.eclipse.jface.dialogs.*;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
 
-public class ViewActionDelegate implements IViewActionDelegate
+public class HelpPreferencesActionDelegate implements IViewActionDelegate
 {
     public IViewPart view;
-    public HelpDialogSetting helpDialog;
+    public HelpPreferencesDialogSetting helpDialog;
 
     public void init(IViewPart view)
     {
 	this.view = view;
     }
 
-    public ViewActionDelegate()
+    public HelpPreferencesActionDelegate()
     {
 	super();
     }
     
     public void run(IAction action)
     {
-	helpDialog = new HelpDialogSetting(view.getSite().getShell());
-	helpDialog.open();
+	helpDialog = new HelpPreferencesDialogSetting(view.getSite().getShell());
+        helpDialog.open();
     }
     
     public void selectionChanged(IAction action, ISelection selection)
