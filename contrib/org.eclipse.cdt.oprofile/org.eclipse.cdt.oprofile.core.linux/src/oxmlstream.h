7bc98741cee83afe577780d1a31a8e984e52d4f2
Jeremy Handcock <jeremy@aperte.org>
2005-02-05 03:33:35 +0000

    * imageheader.cc: Update license header. 	* imageheader.h: Likewise. 	* opinfo.cc: Likewise. 	* opinfo.h: Likewise. 	* oprofiledb.cc: Likewise. 	* oprofiledb.h: Likewise. 	* opxml.cc: Likewise. 	* oxmlstream.h: Likewise. 	* profileimage.cc: Likewise. 	* sample.cc: Likewise. 	* sample.h: Likewise. 	* samplefile.cc: Likewise. 	* samplefile.h: Likewise. 	* session.cc: Likewise. 	* session.h: Likewise. 	* sevent.cc: Likewise. 	* sevent.h: Likewise. 	* stable.cc: Likewise. 	* stable.h: Likewise. 	* symbol.cc: Likewise. 	* symble.h: Likewise. 	* xmlbuf.cc: Likewise. 	* xmlbuf.h: Likewise. 	* xmlfmt.cc: Likewise. 	* xmlfmt.h: Likewise. 	* xmltag.cc: Likewise. 	* xmltag.h: Likewise.

--- contrib/org.eclipse.cdt.oprofile/org.eclipse.cdt.oprofile.core.linux/src/oxmlstream.h
+++ contrib/org.eclipse.cdt.oprofile/org.eclipse.cdt.oprofile.core.linux/src/oxmlstream.h
@@ -2,20 +2,13 @@
    Written by Keith Seitz <keiths@redhat.com>
    Copyright 2003, Red Hat, Inc.
 
-   This program is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2 of the License, or
-   (at your option) any later version.
+   This program is open source software licensed under the Eclipse
+   Public License ver. 1.
 
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 59 Temple Place - Suite 330,
-   Boston, MA 02111-1307, USA.  */
+   Alternatively, this program may be used under the terms of the GNU
+   General Public License as published by the Free Software Foundation;
+   either version 2 of the License, or (at your option) any later
+   version.  */
 
 #ifndef _OXMLSTREAM_H
 #define _OXMLSTREAM_H
