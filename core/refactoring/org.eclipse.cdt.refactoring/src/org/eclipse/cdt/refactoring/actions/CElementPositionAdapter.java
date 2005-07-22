006884c5b149fb347903ca9e8d552dc7f72b5eea
Doug Schaefer <doug.schaefer@windriver.com>
2005-07-22 15:01:47 +0000

    More fun with EPL. I think we have all Java files compliant now.

--- core/refactoring/org.eclipse.cdt.refactoring/src/org/eclipse/cdt/refactoring/actions/CElementPositionAdapter.java
+++ core/refactoring/org.eclipse.cdt.refactoring/src/org/eclipse/cdt/refactoring/actions/CElementPositionAdapter.java
@@ -1,9 +1,13 @@
-/*
- * CElementPositionAdapter.java
- * Created on 09.06.2005
- *
- * Copyright 2005 Wind River Systems Inc. All rights reserved.
- */
+/*******************************************************************************
+ * Copyright (c) 2005 Wind River Systems, Inc. 
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Eclipse Public License v1.0 
+ * which accompanies this distribution, and is available at 
+ * http://www.eclipse.org/legal/epl-v10.html  
+ * 
+ * Contributors: 
+ * Markus Schorn - initial API and implementation 
+ ******************************************************************************/ 
 package org.eclipse.cdt.refactoring.actions;
 
 import org.eclipse.cdt.core.model.*;
