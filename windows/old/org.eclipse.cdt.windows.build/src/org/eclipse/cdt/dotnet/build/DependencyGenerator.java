799e3349e077faaf754e577de9ee79e9bdf146be
Doug Schaefer <doug.schaefer@windriver.com>
2006-08-12 05:09:57 +0000

    Initial C++.Net MBS support.

---
+++ windows/old/org.eclipse.cdt.windows.build/src/org/eclipse/cdt/dotnet/build/DependencyGenerator.java
@@ -0,0 +1,48 @@
+/**********************************************************************
+ * Copyright (c) 2006 QNX Software Systems and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: 
+ *     QNX Software Systems - Initial API and implementation
+ **********************************************************************/
+
+package org.eclipse.cdt.dotnet.build;
+
+import org.eclipse.cdt.managedbuilder.core.IBuildObject;
+import org.eclipse.cdt.managedbuilder.core.IConfiguration;
+import org.eclipse.cdt.managedbuilder.core.ITool;
+import org.eclipse.cdt.managedbuilder.makegen.IManagedDependencyGenerator2;
+import org.eclipse.cdt.managedbuilder.makegen.IManagedDependencyInfo;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.IPath;
+
+/**
+ * @author Doug Schaefer
+ *
+ */
+public class DependencyGenerator implements IManagedDependencyGenerator2 {
+
+	public int getCalculatorType() {
+		return IManagedDependencyGenerator2.TYPE_CUSTOM;
+	}
+
+	public String getDependencyFileExtension(IConfiguration buildContext, ITool tool) {
+		return "";
+	}
+
+	public IManagedDependencyInfo getDependencySourceInfo(IPath source, IBuildObject buildContext, ITool tool, IPath topBuildDirectory) {
+		return null;
+	}
+
+	public IManagedDependencyInfo getDependencySourceInfo(IPath source, IResource resource, IBuildObject buildContext, ITool tool, IPath topBuildDirectory) {
+		return new DependencyCalculator(source, resource, buildContext, tool, topBuildDirectory);
+	}
+
+	public boolean postProcessDependencyFile(IPath dependencyFile, IConfiguration buildContext, ITool tool, IPath topBuildDirectory) {
+		return false;
+	}
+
+}
