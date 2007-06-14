fc5b52bb37bf2ea4d85d00ad3c988e85442390d0
Doug Schaefer <doug.schaefer@windriver.com>
2007-06-14 20:10:14 +0000

    Fix up copyrights.

--- core/refactoring/org.eclipse.cdt.refactoring.tests/src/org/eclipse/cdt/refactoring/tests/RenameVariableTests.java
+++ core/refactoring/org.eclipse.cdt.refactoring.tests/src/org/eclipse/cdt/refactoring/tests/RenameVariableTests.java
@@ -1,13 +1,13 @@
 /*******************************************************************************
- * Copyright (c) 2005 Wind River Systems, Inc.
- * All rights reserved. This program and the accompanying materials 
- * are made available under the terms of the Eclipse Public License v1.0 
- * which accompanies this distribution, and is available at 
- * http://www.eclipse.org/legal/epl-v10.html  
- * 
- * Contributors: 
+ * Copyright (c) 2005, 2007 Wind River Systems, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
  * Markus Schorn - initial API and implementation 
- ******************************************************************************/ 
+ *******************************************************************************/
 
 package org.eclipse.cdt.refactoring.tests;
 
