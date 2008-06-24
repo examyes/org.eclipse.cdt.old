63235cc6050e132d2de4d18e42964b5d21999895
Doug Schaefer <doug.schaefer@windriver.com>
2008-06-24 02:52:40 +0000

    Started hooking up the launch to the standard debug model which then drives the Windows debug engine.

--- windows/org.eclipse.cdt.msw.debug.core/src/org/eclipse/cdt/msw/debug/core/launch/WinAppLaunchConfigDelegate.java
+++ windows/org.eclipse.cdt.msw.debug.core/src/org/eclipse/cdt/msw/debug/core/launch/WinAppLaunchConfigDelegate.java
@@ -6,13 +6,15 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.debug.core.model.ILaunchConfigurationDelegate;
+import org.eclipse.debug.core.model.IProcess;
 
 public class WinAppLaunchConfigDelegate implements ILaunchConfigurationDelegate {
 
 	@Override
 	public void launch(ILaunchConfiguration configuration, String mode,
 			ILaunch launch, IProgressMonitor monitor) throws CoreException {
-		new WinProcess("notepad", null, launch);
+		IProcess process = new WinProcess("notepad", null, launch);
+		launch.addProcess(process);
 	}
 
 }
