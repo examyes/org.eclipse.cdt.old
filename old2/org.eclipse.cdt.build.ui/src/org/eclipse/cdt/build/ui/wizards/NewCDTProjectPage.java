efd99ced412f25318031036e44d1176bab703219
Doug Schaefer <doug.schaefer@windriver.com>
2009-11-10 20:00:57 +0000

    Prototype CDT build system.

---
+++ old2/org.eclipse.cdt.build.ui/src/org/eclipse/cdt/build/ui/wizards/NewCDTProjectPage.java
@@ -0,0 +1,82 @@
+/*******************************************************************************
+ * Copyright (c) 2009 Wind River Systems and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Wind River Systems - Initial API and implementation
+ *******************************************************************************/
+package org.eclipse.cdt.build.ui.wizards;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.List;
+import org.eclipse.ui.dialogs.WizardNewProjectCreationPage;
+
+public class NewCDTProjectPage extends WizardNewProjectCreationPage {
+
+	String[][] testBuildSystems = new String[][] {
+			{ "make", "User provides Makefile that guides the build." },
+			{ "managed make", "CDT generates the Makefile for the build." },
+			{ "autoconf", "Use the GNU autoconf build system." },
+			{ "other", "User specifies which external build system to use." }
+	};
+	
+	public NewCDTProjectPage(String pageName) {
+		super(pageName);
+	}
+
+	@Override
+	public void createControl(Composite parent) {
+		super.createControl(parent);
+		
+		Composite composite = (Composite)getControl();
+		
+		setTitle("CDT Project");
+		setMessage("Specify name, location, and build system for this project.");
+		
+		Group buildSystemGroup = new Group(composite, SWT.NONE);
+		GridLayout layout = new GridLayout();
+		layout.numColumns = 2;
+		layout.makeColumnsEqualWidth = true;
+		buildSystemGroup.setLayout(layout);
+		buildSystemGroup.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+		buildSystemGroup.setText("Build System");
+		
+		final List buildSystems = new List(buildSystemGroup, SWT.SINGLE);
+		GridData layoutData = new GridData(SWT.FILL, SWT.FILL, true, true);
+		buildSystems.setLayoutData(layoutData);
+		
+		final Label label = new Label(buildSystemGroup, SWT.WRAP);
+		label.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+		
+		for (String[] system : testBuildSystems) {
+			buildSystems.add(system[0]);
+		}
+		
+		buildSystems.addSelectionListener(new SelectionListener() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				int i = buildSystems.getSelectionIndex();
+				label.setText(testBuildSystems[i][1]);
+			}
+			
+			@Override
+			public void widgetDefaultSelected(SelectionEvent e) {
+				widgetSelected(e);
+			}
+		});
+		
+		buildSystems.setSelection(0);
+		label.setText(testBuildSystems[0][1]);
+	}
+	
+}
---
+++ old2/org.eclipse.cdt.build.ui/src/org/eclipse/cdt/build/ui/wizards/NewCDTProjectWizard.java
@@ -0,0 +1,37 @@
+/*******************************************************************************
+ * Copyright (c) 2009 Wind River Systems and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Wind River Systems - Initial API and implementation
+ *******************************************************************************/
+package org.eclipse.cdt.build.ui.wizards;
+
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.ui.INewWizard;
+import org.eclipse.ui.IWorkbench;
+
+public class NewCDTProjectWizard extends Wizard implements INewWizard {
+
+	@Override
+	public boolean performFinish() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void init(IWorkbench workbench, IStructuredSelection selection) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void addPages() {
+		addPage(new NewCDTProjectPage("New CDT Project"));
+	}
+	
+}
